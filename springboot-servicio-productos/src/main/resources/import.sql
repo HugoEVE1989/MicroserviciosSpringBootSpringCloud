2d54487da52f04b5a731ede895d494c607e86c59
Hugo Eloy <hugo.eve.89@gmail.com>
2019-09-30 22:17:59 -0500

    Se actualiza con proyecto hasta el capitulo 12

---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony', 700, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple', 1800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Nike', 900, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Adidas', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Bianchi', 500, NOW());
